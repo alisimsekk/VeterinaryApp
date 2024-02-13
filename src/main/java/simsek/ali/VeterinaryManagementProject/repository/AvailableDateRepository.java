af0da807c8f8e931e99562b3236db3c4636e73b7
alisimsekk <ali.simsek035@gmail.com>
2024-02-14 02:56:05 +0300

    Bug fixed

--- src/main/java/simsek/ali/VeterinaryManagementProject/repository/AvailableDateRepository.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/repository/AvailableDateRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface AvailableDateRepository extends JpaRepository<AvailableDate, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT ad1_0.id, ad1_0.available_date, ad1_0.doctor_id FROM available_date ad1_0 LEFT JOIN doctor d1_0 ON d1_0.id = ad1_0.doctor_id WHERE ad1_0.available_date = ?1 AND ad1_0.doctor_id = ?2 OR d1_0.email =?3")
+    @Query(nativeQuery = true, value = "SELECT ad1_0.id, ad1_0.available_date, ad1_0.doctor_id FROM available_date ad1_0 LEFT JOIN doctor d1_0 ON d1_0.id = ad1_0.doctor_id WHERE ad1_0.available_date = ?1 AND (ad1_0.doctor_id = ?2 OR d1_0.email =?3)")
     Optional<AvailableDate> findAvailableDateAndDoctorIdOrDoctorEmail(LocalDate availableDate, Long id, String email);
 
 
