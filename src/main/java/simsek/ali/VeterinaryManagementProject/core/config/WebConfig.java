77c056a5767b4fa05fa67b6da755965fa2be04a6
alisimsekk <ali.simsek035@gmail.com>
2024-02-14 15:41:00 +0300

    Web Config was updated

--- src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
@@ -1,15 +1,20 @@
 package simsek.ali.VeterinaryManagementProject.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${vetapp.host}")
+    private String vetAppHost;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(vetAppHost)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
